a7cbf51bdab75605322d76b9fd997f2d906c3fd8
Miha <miromanovsky@gmail.com>
2023-07-25 12:44:51 +0800

    Initial Commit

---
+++ CamFilterApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CamFilterApp
+//
+//  Created by Mikhayl Romanovsky on 2023/7/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
